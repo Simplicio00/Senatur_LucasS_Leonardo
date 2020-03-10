607f2be18343c487068183153f7d7c904e82b26a
Leonardorichylo <leonardorichy@outlook.com.br>
2020-03-10 10:04:47 -0300

    Modelando o banco via codigo

--- api/SenaTur.Senai.WebApi/SenaTur.Senai.WebApi/Contexts/Context.cs
+++ api/SenaTur.Senai.WebApi/SenaTur.Senai.WebApi/Contexts/Context.cs
@@ -1,11 +1,54 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SenaTur.Senai.WebApi.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SenaTur.Senai.WebApi.Contexts
 {
-    public class Context
+    public class Context : DbContext
     {
+        public DbSet<TiposUsuario> TiposUsuario { get; set; }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        public DbSet<Pacotes> Pacotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=N-1S-DEV-14\\SQLEXPRESS; Database=SenaTur_Senai_Manha; user Id=sa; pwd=sa@132;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TiposUsuario>(entity =>
+            {
+                entity
+                .HasIndex(tu => tu.Titulo)
+                .IsUnique();
+                entity.HasData(
+                    new Domains.TiposUsuario
+                    {
+                        IdTipoUsuario = 1,
+                        Titulo = "Administrador"
+                    },
+                    new TiposUsuario
+                    {
+                        IdTipoUsuario = 2,
+                        Titulo = "Cliente"
+                    });
+            });
+
+            modelBuilder.Entity<Usuarios>().HasData(
+                new Usuarios
+                {
+                    IdUsuario = 1,
+                    Email = "admin@admim.com",
+                    Senha = "admin",
+                    Id
+                })
+        }
     }
 }
