d3e6b3d082129eddb5fdeff64d7445c3fd28470b
Leonardorichylo <leonardorichy@outlook.com.br>
2020-03-10 10:39:25 -0300

    modelagem por codigo completa

--- api/SenaTur.Senai.WebApi/SenaTur.Senai.WebApi/Contexts/Context.cs
+++ api/SenaTur.Senai.WebApi/SenaTur.Senai.WebApi/Contexts/Context.cs
@@ -47,8 +47,51 @@ namespace SenaTur.Senai.WebApi.Contexts
                     IdUsuario = 1,
                     Email = "admin@admim.com",
                     Senha = "admin",
-                    Id
-                })
+                    IdTipoUsuario = 1
+                },
+                new Usuarios
+                {
+                    IdUsuario = 2,
+                    Email = "cliente@cliente.com",
+                    Senha = "cliente",
+                    IdTipoUsuario = 2
+                });
+
+            modelBuilder.Entity<Pacotes>().HasData(
+                new Pacotes
+                {
+                    IdPacote = 1,
+                    NomePacote = "Salvador - 5 Dias/4 Diárias",
+                    Descricao = " O que não falta em Salvador são atrações. Prova disso são as praias, os museus e as construções seculares que dão um charme mais que especial à região. A cidade, sinônimo de alegria, também é conhecida pela efervescência cultural que a credenciou como um dos destinos mais procurados por turistas brasileiros e estrangeiros. O Pelourinho e o Elevador são alguns dos principais pontos de visitação.",
+                    DataIda = Convert.ToDateTime("06/08/2020"),
+                    DataVolta = Convert.ToDateTime("10/08/2020"),
+                    Valor =  Convert.ToDecimal("854.00"),
+                    NomeCidade = "Salvador",
+                    Ativo = Convert.ToBoolean(1)
+},
+                new Pacotes
+                {
+                    IdPacote = 2,
+                    NomePacote = "Resorts na Bahia - Litoral Norte - 5 dias/4 diárias",
+                    Descricao = "O Litoral Norte da Bahia conta com inúmeras praias emolduradas por coqueiros, além de piscinas naturais de águas mornas que são protegidas por recifes e habitadas por peixes coloridos. Banhos de mar em águas calmas ou agitadas, mergulho com snorkel, caminhada pela orla e calçadões, passeios de bicicleta, pontos turísticos históricos, interação com animais e até baladas estão entre as atrações da região. Destacam-se as praias de Guarajuba, Imbassaí, Praia do Forte e Costa do Sauipe.",
+                    DataIda = Convert.ToDateTime("14/05/2020"),
+                    DataVolta = Convert.ToDateTime("18/05/2020"),
+                    Valor = Convert.ToDecimal("1826.00"),
+                    NomeCidade = "Salvador",
+                    Ativo = Convert.ToBoolean(1)
+                },
+                new Pacotes
+                {
+                    IdPacote = 3,
+                    NomePacote = "Bonito via Campo Grande - 1 Passeio - 5 dias/4 diárias",
+                    Descricao = "Localizado no estado de Mato Grosso do Sul e ao sul do Pantanal, Bonito possui centenas de cachoeiras, rios e lagos de águas cristalinas, além de cavernas inundadas, paredões rochosos e uma infinidade de peixes. Os aventureiros costumam render-se facilmente a esse destino regado por trilhas ecológicas, passeios de bote e descidas de rapel pelas inúmeras quedas d'água da região",
+                    DataIda = Convert.ToDateTime("28/03/2020"),
+                    DataVolta = Convert.ToDateTime("01/04/2020"),
+                    Valor = Convert.ToDecimal("1004.00"),
+                    NomeCidade = "Bonito",
+                    Ativo = Convert.ToBoolean(1)
+                });
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ api/SenaTur.Senai.WebApi/SenaTur.Senai.WebApi/Migrations/20200310133700_SenaTur_Senai_Manha.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SenaTur.Senai.WebApi.Migrations
+{
+    public partial class SenaTur_Senai_Manha : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jogos",
+                columns: table => new
+                {
+                    IdPacote = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NomePacote = table.Column<string>(type: "VARCHAR(150)", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false),
+                    DataIda = table.Column<DateTime>(type: "DATE", nullable: false),
+                    DataVolta = table.Column<DateTime>(type: "DATE", nullable: false),
+                    Preco = table.Column<decimal>(type: "DECIMAL (18,2)", nullable: false),
+                    Ativo = table.Column<bool>(type: "Bit", nullable: false),
+                    NomeCidade = table.Column<string>(type: "VARCHAR(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jogos", x => x.IdPacote);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TiposUsuario",
+                columns: table => new
+                {
+                    IdTipoUsuario = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Titulo = table.Column<string>(type: "VARCHAR(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposUsuario", x => x.IdTipoUsuario);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(type: "VARCHAR(150)", nullable: false),
+                    Senha = table.Column<string>(type: "VARCHAR(150)", maxLength: 30, nullable: false),
+                    IdTipoUsuario = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.IdUsuario);
+                    table.ForeignKey(
+                        name: "FK_Usuarios_TiposUsuario_IdTipoUsuario",
+                        column: x => x.IdTipoUsuario,
+                        principalTable: "TiposUsuario",
+                        principalColumn: "IdTipoUsuario",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Jogos",
+                columns: new[] { "IdPacote", "Ativo", "DataIda", "DataVolta", "Descricao", "NomeCidade", "NomePacote", "Preco" },
+                values: new object[,]
+                {
+                    { 1, true, new DateTime(2020, 8, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 8, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), " O que não falta em Salvador são atrações. Prova disso são as praias, os museus e as construções seculares que dão um charme mais que especial à região. A cidade, sinônimo de alegria, também é conhecida pela efervescência cultural que a credenciou como um dos destinos mais procurados por turistas brasileiros e estrangeiros. O Pelourinho e o Elevador são alguns dos principais pontos de visitação.", "Salvador", "Salvador - 5 Dias/4 Diárias", 85400m },
+                    { 2, true, new DateTime(2020, 5, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 5, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), "O Litoral Norte da Bahia conta com inúmeras praias emolduradas por coqueiros, além de piscinas naturais de águas mornas que são protegidas por recifes e habitadas por peixes coloridos. Banhos de mar em águas calmas ou agitadas, mergulho com snorkel, caminhada pela orla e calçadões, passeios de bicicleta, pontos turísticos históricos, interação com animais e até baladas estão entre as atrações da região. Destacam-se as praias de Guarajuba, Imbassaí, Praia do Forte e Costa do Sauipe.", "Salvador", "Resorts na Bahia - Litoral Norte - 5 dias/4 diárias", 182600m },
+                    { 3, true, new DateTime(2020, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2020, 4, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Localizado no estado de Mato Grosso do Sul e ao sul do Pantanal, Bonito possui centenas de cachoeiras, rios e lagos de águas cristalinas, além de cavernas inundadas, paredões rochosos e uma infinidade de peixes. Os aventureiros costumam render-se facilmente a esse destino regado por trilhas ecológicas, passeios de bote e descidas de rapel pelas inúmeras quedas d'água da região", "Bonito", "Bonito via Campo Grande - 1 Passeio - 5 dias/4 diárias", 100400m }
+                });
+
+            migrationBuilder.InsertData(
+                table: "TiposUsuario",
+                columns: new[] { "IdTipoUsuario", "Titulo" },
+                values: new object[,]
+                {
+                    { 1, "Administrador" },
+                    { 2, "Cliente" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "IdUsuario", "Email", "IdTipoUsuario", "Senha" },
+                values: new object[] { 1, "admin@admim.com", 1, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "IdUsuario", "Email", "IdTipoUsuario", "Senha" },
+                values: new object[] { 2, "cliente@cliente.com", 2, "cliente" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TiposUsuario_Titulo",
+                table: "TiposUsuario",
+                column: "Titulo",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_IdTipoUsuario",
+                table: "Usuarios",
+                column: "IdTipoUsuario");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jogos");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "TiposUsuario");
+        }
+    }
+}
