c0aa0785579d6ddda281907086b18d38a7050532
Leonardorichylo <leonardorichy@outlook.com.br>
2020-03-10 09:07:47 -0300

    Criando domains

---
+++ api/SenaTur.Senai.WebApi/SenaTur.Senai.WebApi/Domains/Pacotes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenaTur.Senai.WebApi.Domains
+{
+    [Table("Jogos")]
+    public class Pacotes
+    {
+
+        [Key]
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdPacote { get; set; }
+
+        [Column(TypeName = "VARCHAR(150)")]
+
+        //IDPacote,NomePacote,Descricao,DataIda,DataVolta,Valor,Ativo e NomeCidade
+    }
+}
