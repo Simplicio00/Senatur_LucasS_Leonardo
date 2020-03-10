e92cf15010633da5d17c34d84d1ac27794ca2a91
Lucas Simplicio de Oliveira <47944333873@info.local>
2020-03-10 09:01:12 -0300

    swagger pronto

--- api/SenaTur.Senai.WebApi/SenaTur.Senai.WebApi/Startup.cs
+++ api/SenaTur.Senai.WebApi/SenaTur.Senai.WebApi/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace SenaTur.Senai.WebApi
 {
@@ -15,6 +18,14 @@ namespace SenaTur.Senai.WebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SenaTur.Senai.WebApi", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
         }
 
@@ -25,6 +36,13 @@ namespace SenaTur.Senai.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SenaTur.Senai.WebApi");
+            });
 
             app.UseMvc();
         }
